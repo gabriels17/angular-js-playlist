66ec102fa0017e014cb4473263c63c4cb9583b74
gabriels17 <gabriel.sighvatsson@gmail.com>
2018-12-28 02:01:24 +0000

    Added a remove ninja function, header and styles

--- app/app.js
+++ app/app.js
@@ -9,3 +9,43 @@ var myNinjaApp = angular.module('myNinjaApp', []);
 //     // Fires when the application runs
 // });
 
+// myNinjaApp.controller('NinjaController', function($scope){
+// Variable names sometimes need to be protected from minification
+myNinjaApp.controller('NinjaController', ['$scope', function($scope){
+
+    // The scope object is the binding between
+    // the HTML view and the JS controller
+
+    $scope.removeNinja = function(ninja){
+        var removedNinja = $scope.ninjas.indexOf(ninja);
+        $scope.ninjas.splice(removedNinja, 1);
+    }
+
+    $scope.ninjas = [
+        {
+            name: 'Yoshi',
+            belt: 'green',
+            rate: 50,
+            available: true
+        },
+        {
+            name: 'Crystal',
+            belt: 'yellow',
+            rate: 30,
+            available: true
+        },
+        {
+            name: 'Ryu',
+            belt: 'orange',
+            rate: 10,
+            available: false
+        },
+        {
+            name: 'Shaun',
+            belt: 'black',
+            rate: 1000,
+            available: true
+        }
+    ];
+
+}]);
