57b2f8850d89eacbdf6b76b56650e4473deffb56
gabriels17 <gabriel.sighvatsson@gmail.com>
2018-12-28 01:17:51 +0000

    Created a module in app.js

---
+++ app/app.js
@@ -0,0 +1,11 @@
+var myNinjaApp = angular.module('myNinjaApp', []);
+
+// myNinjaApp.config(function(){
+//     // Preparation
+//     // Runs before the application runs
+// });
+
+// myNinjaApp.run(function(){
+//     // Fires when the application runs
+// });
+
