1d830b4e457f69573c4ee49384acddfee8295986
gabriels17 <gabriel.sighvatsson@gmail.com>
2019-01-09 22:16:33 +0000

    Added some basic animations

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute', 'ngAnimate']);
 
 // Preparation - runs before the application runs
 myNinjaApp.config(['$routeProvider', function($routeProvider){
@@ -30,6 +30,8 @@ myNinjaApp.directive('randomNinja', [function(){
             title: '='
         },
         templateUrl: 'views/random.html',
+        transclude: true,
+        replace: true,
         controller: function($scope){
             $scope.random = Math.floor(Math.random() * 4);
         }
